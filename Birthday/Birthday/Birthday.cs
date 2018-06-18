e9c6bdd6fbda81d3f78800a0c7c05f86a9f8d350
su20 <19liuq@elmbrookstudents.org>
2018-06-18 09:33:24 -0500

    initial project

---
+++ Birthday/Birthday/Birthday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birthday
+{
+    public class Birthday
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
