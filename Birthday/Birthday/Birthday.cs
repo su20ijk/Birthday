61e3b071be3d27c47463a1a65e00009910bd25f6
Andrew Kim <19kima@elmbrookstudents.org>
2018-06-18 10:00:24 -0500

    andrew's 2nd birthday

--- Birthday/Birthday/Birthday.cs
+++ Birthday/Birthday/Birthday.cs
@@ -14,6 +14,7 @@ namespace Birthday
             Console.WriteLine("nobody can not say yeet");
             Console.WriteLine("More drawing supplies.");
             Console.WriteLine("A PONYEEEEEEEEEEEEEEEEEEEEEEEEEEEeT");
+            Console.WriteLine("a pale green polo");
             Console.ReadKey();
         }
     }
